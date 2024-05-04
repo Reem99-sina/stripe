49cfed5f4c31028bae4ba0f7e264ebe5bcbfe33b
Reem99-sina <reemsina2@gmail.com>
2024-05-04 09:21:31 +0300

    new stripe

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express=require("express");
 const { connectdb } = require("./connect");
-const { requestStripe } = require("./stripeConnect");
+const { requestStripe, createPaymentIntent } = require("./stripeConnect");
 const app=express()
 require('dotenv').config();
 app.use(express.json())
 // connectdb()
 app.post("/payment-sheet",requestStripe)
+app.post("/create-payment-intent",createPaymentIntent)
 app.listen(3500,()=>{
     console.log("app listen")
 })
--- stripeConnect.js
+++ stripeConnect.js
@@ -20,7 +20,19 @@ module.exports.requestStripe=async(req,res)=>{
     paymentIntent: paymentIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.id,
-    publishableKey: 'pk_test_TYooMQauvdEDq54NiTphI7jx'
+    publishableKey: 'pk_test_51OnViJJ1yUQMPxwpHSfb0uZFYHJwEo0VBpr3AIRmGwdw6tli8fYgSIVHNn3IE6ZiPpaqplfmaJOx3vuBwaEnn6Kj00Psw1Yqxu'
   });
   
+}
+module.exports.createPaymentIntent=async (req, res) => {
+  const { items } = req.body;
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items),
+    currency: "usd"
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret
+  })
 }
